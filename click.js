856761826c643390b85e8454086d7a386cb69e45
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-08-19 10:36:10 +0900

    コメントアウト追記

--- click.js
+++ click.js
@@ -1,8 +1,11 @@
+//click.htmlと連携
+//getElementByIdの演習
 function myFunction() {
   const e = document.getElementById("mySelect").value;
   document.getElementById("demo").innerHTML = "あなたの選んだ色は" + e + "色です。";
 }
 
+//addEventListenerの演習
 const input = document.querySelector("input");
 const history = document.getElementById("history");
 
@@ -11,3 +14,4 @@ input.addEventListener("change", updateValue);
 function updateValue(i) {
   history.textContent = i.target.value;
 }
+
