7b357e1b223af661cefbb1feaa95d2b1bc5ef483
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-08-19 10:33:22 +0900

    AddEventListener練習

--- click.js
+++ click.js
@@ -3,3 +3,11 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "あなたの選んだ色は" + e + "色です。";
 }
 
+const input = document.querySelector("input");
+const history = document.getElementById("history");
+
+input.addEventListener("change", updateValue);
+
+function updateValue(i) {
+  history.textContent = i.target.value;
+}
