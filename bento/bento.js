0d2e60af1044814b48a9173838ef0f56fa8ae324
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-08-24 09:36:53 +0900

    nameでも受け取れるとコメントアウトを記載

--- bento/bento.js
+++ bento/bento.js
@@ -1,7 +1,7 @@
 const OPTION_ANSWER = document.getElementById("optionAnswer");
 
 function riceButtonClick () {
-  const RICE_INPUT = document.riceForm.rice
+  const RICE_INPUT = document.riceForm.rice //又はdocument.getElementsByName("rice")
   let rice_output;
   for (let i = 0; i<RICE_INPUT.length; i++) {
     if (RICE_INPUT[i].checked) {
