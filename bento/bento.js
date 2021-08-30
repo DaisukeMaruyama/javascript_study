4f1e1562fc4b6c95ae6e79006f5b40682f264d9e
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-08-30 19:22:02 +0900

    ショッピングカートお弁当追加機能

--- bento/bento.js
+++ bento/bento.js
@@ -38,7 +38,7 @@ const CART_CONFIRM = document.getElementById("cartConfirm");
 const CART_LIST = document.getElementById("cartList");
 
 CART_CONFIRM.onclick = () => {
-  const LI = `<li>・${NAME.textContent}</li>`;
+  const LI = `<li class="mt-1">${NAME.textContent}</li>`;
   CART_LIST.insertAdjacentHTML( `beforeend`, LI);
 }
 
